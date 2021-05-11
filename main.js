3f21391e194ee53d4c1d66576501eb9bd86fb40e
Ballo Adam <ballo.adam@yahoo.com>
2021-05-11 18:47:10 +0300

    resnponsive

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const sections = {
      home: {
           position: 0
@@ -15,40 +14,72 @@ const contactBtn = document.querySelector('#contact-btn');
 const contactBtn_2 = document.querySelector('#contact-btn-2');
 aboutMeBtn.addEventListener('click', (event) => {
      event.preventDefault();
- 
-     window.scrollBy({
-         top: window.innerHeight * sections.about.position,
-         left: 0,
-         behavior: 'smooth'
-     })
- })
-contactBtn.addEventListener('click', (event) => {
-    event.preventDefault();
 
-    window.scrollBy({
-        top: window.innerHeight * sections.contact.position,
-        left: 0,
-        behavior: 'smooth'
-    })
-})
+     window.scrollBy({
+          top: window.innerHeight * sections.about.position,
+          left: 0,
+          behavior: 'smooth'
+     })
+});
+contactBtn.addEventListener('click', (event) => {
+     event.preventDefault();
+
+     window.scrollBy({
+          top: window.innerHeight * sections.contact.position,
+          left: 0,
+          behavior: 'smooth'
+     })
+});
 contactBtn_2.addEventListener('click', (event) => {
      event.preventDefault();
- 
+
      window.scrollBy({
-         top: window.innerHeight * sections.contact.position,
-         left: 0,
-         behavior: 'smooth'
+          top: window.innerHeight * sections.contact.position,
+          left: 0,
+          behavior: 'smooth'
      })
- })
+});
 
 const backToTop = document.querySelector('#back-to-top');
-console.log(backToTop)
 backToTop.addEventListener('click', (event) => {
      event.preventDefault();
- 
+
      window.scrollBy({
-         top: window.innerHeight * 2 * -1,
-         left: 0,
-         behavior: 'smooth'
-     })
- })
+          top: window.innerHeight * 2 * -1,
+          left: 0,
+          behavior: 'smooth'
+     });
+});
+
+let canScroll = true;
+document.addEventListener('wheel', (e) => {
+     if (!canScroll)
+          return;
+     if (e.deltaY > 0) {
+          canScroll = false;
+          window.scrollBy({
+               top: window.innerHeight,
+               left: 0,
+               behavior: 'smooth'
+          });
+
+          initEnableScroll();
+          return;
+     }
+
+     if (e.deltaY < 0) {
+          canScroll = false;
+          window.scrollBy({
+               top: window.innerHeight * -1,
+               left: 0,
+               behavior: 'smooth'
+          });
+
+          initEnableScroll();
+          return;
+     }
+});
+
+function initEnableScroll() {
+     setTimeout(() => canScroll = true, 800);
+}
