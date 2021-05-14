bbcab1465fe322b72a48b8349427dee4aafa8e66
Adam Ballo <adam.ballo@accesa.eu>
2021-05-14 10:08:24 +0300

    added copyright; edited contact style font size;

--- main.js
+++ main.js
@@ -1,85 +1,92 @@
 const sections = {
-     home: {
-          position: 0
-     },
-     about: {
-          position: 1
-     },
-     contact: {
-          position: 2
-     }
+    home: {
+        position: 0
+    },
+    about: {
+        position: 1
+    },
+    contact: {
+        position: 2
+    }
 };
 const aboutMeBtn = document.querySelector('#about-me-btn');
 const contactBtn = document.querySelector('#contact-btn');
 const contactBtn_2 = document.querySelector('#contact-btn-2');
 aboutMeBtn.addEventListener('click', (event) => {
-     event.preventDefault();
+    event.preventDefault();
 
-     window.scrollBy({
-          top: window.innerHeight * sections.about.position,
-          left: 0,
-          behavior: 'smooth'
-     })
+    window.scrollBy({
+        top: window.innerHeight * sections.about.position,
+        left: 0,
+        behavior: 'smooth'
+    })
 });
 contactBtn.addEventListener('click', (event) => {
-     event.preventDefault();
+    event.preventDefault();
 
-     window.scrollBy({
-          top: window.innerHeight * sections.contact.position,
-          left: 0,
-          behavior: 'smooth'
-     })
+    window.scrollBy({
+        top: window.innerHeight * sections.contact.position,
+        left: 0,
+        behavior: 'smooth'
+    })
 });
 contactBtn_2.addEventListener('click', (event) => {
-     event.preventDefault();
+    event.preventDefault();
 
-     window.scrollBy({
-          top: window.innerHeight * sections.contact.position,
-          left: 0,
-          behavior: 'smooth'
-     })
+    window.scrollBy({
+        top: window.innerHeight * sections.contact.position,
+        left: 0,
+        behavior: 'smooth'
+    })
 });
 
 const backToTop = document.querySelector('#back-to-top');
 backToTop.addEventListener('click', (event) => {
-     event.preventDefault();
+    event.preventDefault();
 
-     window.scrollBy({
-          top: window.innerHeight * 2 * -1,
-          left: 0,
-          behavior: 'smooth'
-     });
+    window.scrollBy({
+        top: window.innerHeight * 2 * -1,
+        left: 0,
+        behavior: 'smooth'
+    });
 });
 
 let canScroll = true;
 document.addEventListener('wheel', (e) => {
-     if (!canScroll)
-          return;
-     if (e.deltaY > 0) {
-          canScroll = false;
-          window.scrollBy({
-               top: window.innerHeight,
-               left: 0,
-               behavior: 'smooth'
-          });
+    if (!canScroll)
+        return;
+    if (e.deltaY > 0) {
+        canScroll = false;
+        window.scrollBy({
+            top: window.innerHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
 
-          initEnableScroll();
-          return;
-     }
+        initEnableScroll();
+        return;
+    }
 
-     if (e.deltaY < 0) {
-          canScroll = false;
-          window.scrollBy({
-               top: window.innerHeight * -1,
-               left: 0,
-               behavior: 'smooth'
-          });
+    if (e.deltaY < 0) {
+        canScroll = false;
+        window.scrollBy({
+            top: window.innerHeight * -1,
+            left: 0,
+            behavior: 'smooth'
+        });
 
-          initEnableScroll();
-          return;
-     }
+        initEnableScroll();
+        return;
+    }
 });
 
 function initEnableScroll() {
-     setTimeout(() => canScroll = true, 800);
+    setTimeout(() => canScroll = true, 800);
+}
+
+const cRightYear = document.querySelector('#copyright_year');
+if (cRightYear) {
+    cRightYear.innerHTML = new Date().getFullYear();
+} else {
+    cRightYear.innerHTML = 2021;
 }
