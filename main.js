4aca51f661be2a2f8a12edf968cd7b8ca88fe3a8
Ballo Adam <ballo.adam@yahoo.com>
2021-10-05 11:26:45 +0300

    changed header; added hamburger; change about me image; change contact email address; changed header font size; added subscribe button; added new-song page

--- main.js
+++ main.js
@@ -1,49 +1,99 @@
 const sections = {
-    home: {
-        position: 0
-    },
-    about: {
-        position: 1
-    },
-    contact: {
-        position: 2
-    }
+     home: {
+          position: 0
+     },
+     newSong: {
+          position: 1
+     },
+     about: {
+          position: 2
+     },
+     contact: {
+          position: 3
+     }
 };
+const newSontBtn = document.querySelector('#new-song-btn');
 const aboutMeBtn = document.querySelector('#about-me-btn');
 const contactBtn = document.querySelector('#contact-btn');
-aboutMeBtn.addEventListener('click', (event) => {
-    event.preventDefault();
+const newSontBtn_h = document.querySelector('#new-song-btn-h');
+const aboutMeBtn_h = document.querySelector('#about-me-btn-h');
+const contactBtn_h = document.querySelector('#contact-btn-h');
 
-    window.scrollTo({
-        top: window.innerHeight * sections.about.position,
+newSontBtn.addEventListener('click', (event) => {
+     event.preventDefault();
+     window.scrollTo({
+        top: window.innerHeight * sections.newSong.position,
         left: 0,
-        behavior: 'smooth'
-    })
+        behavior: 'smooth'  
+     });
 });
-contactBtn.addEventListener('click', (event) => {
-    event.preventDefault();
 
-    window.scrollTo({
-        top: window.innerHeight * sections.contact.position,
+newSontBtn_h.addEventListener('click', (event) => {
+     event.preventDefault();
+     window.scrollTo({
+        top: window.innerHeight * sections.newSong.position,
         left: 0,
-        behavior: 'smooth'
-    })
+        behavior: 'smooth'  
+     });
+     triggerHamburgerMenu();
+});
+
+aboutMeBtn.addEventListener('click', (event) => {
+     event.preventDefault();
+
+     window.scrollTo({
+          top: window.innerHeight * sections.about.position,
+          left: 0,
+          behavior: 'smooth'
+     })
+});
+
+aboutMeBtn_h.addEventListener('click', (event) => {
+     event.preventDefault();
+
+     window.scrollTo({
+          top: window.innerHeight * sections.about.position,
+          left: 0,
+          behavior: 'smooth'
+     })
+     triggerHamburgerMenu();
+});
+
+contactBtn.addEventListener('click', (event) => {
+     event.preventDefault();
+
+     window.scrollTo({
+          top: window.innerHeight * sections.contact.position,
+          left: 0,
+          behavior: 'smooth'
+     })
+});
+
+contactBtn_h.addEventListener('click', (event) => {
+     event.preventDefault();
+
+     window.scrollTo({
+          top: window.innerHeight * sections.contact.position,
+          left: 0,
+          behavior: 'smooth'
+     })
+     triggerHamburgerMenu();
 });
 
 const backToTop = document.querySelector('.header-brand');
 backToTop.addEventListener('click', (event) => {
-    event.preventDefault();
+     event.preventDefault();
 
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-    });
+     window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+     });
 });
 
 const cRightYear = document.querySelector('#copyright_year');
 if (cRightYear) {
-    cRightYear.innerHTML = new Date().getFullYear();
+     cRightYear.innerHTML = new Date().getFullYear();
 } else {
-    cRightYear.innerHTML = 2021;
+     cRightYear.innerHTML = 2021;
 }
---
+++ scripts/hamburger.js
@@ -0,0 +1,8 @@
+function triggerHamburgerMenu() {
+     var x = document.getElementById("hamburger-menu");
+     if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+}
